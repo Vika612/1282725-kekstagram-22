33e343e772b392b62a2ffe8413790149597c1aaa
Victoria Domovets <v.domovets@gmail.com>
2021-01-30 16:49:25 +0100

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+
 const getRandomInteger = function (min, max) {
-  if (min >= 0 && max >= 0) {
+  if (min >= 0 && max >= 0 && min < max) {
     return Math.round(Math.random() * (max - min + 1) + min);
   }
+  window.console.error('error message');
 };
+getRandomInteger(0, 100);
 
 
-const getCharacterLength = function (str, max) {
+// Функция для проверки максимальной длины строки.
+
+const getCharacterLength = function (str, max = 140) {
   return str.length <= max;
 };
+getCharacterLength('');
