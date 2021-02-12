ba3fbba7913988a87be68592d49675252577b20f
Victoria Domovets <v.domovets@gmail.com>
2021-02-12 12:08:02 +0100

    module4-task1

---
+++ js/util.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const MAX_STR_LENGTH = 140;
+
+const getRandomInteger = (min, max) => {
+  if (min >= 0 && min < max) {
+    return Math.round(Math.random() * (max - min + 1) + min);
+  }
+  window.console.error('error message');
+};
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+const getCharacterLength = (str, max = MAX_STR_LENGTH) => {
+  return str.length <= max;
+};
+getCharacterLength('');
+
+
+export {getRandomInteger, getRandomArrayElement, getCharacterLength};
