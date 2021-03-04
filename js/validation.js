fdcfd95b8c93548a1a90fc8b7ee8e47b618f04cd
Victoria Domovets <v.domovets@gmail.com>
2021-03-04 15:57:17 +0100

    module6-task2 fix1

--- js/validation.js
+++ js/validation.js
@@ -5,17 +5,29 @@ const hashtagInput = document.querySelector('.text__hashtags');
 const commentText = document.querySelector('.text__description');
 const hashtagPattern = /ˆ#[A-Za-zА-Яа-я0-9]/;
 
+const isHashtagEqual = (hashtags) => {
+  for (let i = 0; i < hashtags.length - 1; i++) {
+    if (hashtags[i] === hashtags[i + 1]) {
+      return true;
+    }
+  }
+};
+
 const validateHashtag = () => {
   const hashtags = hashtagInput.value.trim().split(' ');
+
   hashtags.forEach((item, array) => {
-    if (!hashtagPattern(item)) {
-      hashtagInput.setCustomValidity('Хэштег должен начинаться с # и содержать только буквы и цифры');
-    }
-    else if (item.length > MAX_HASHTAG_LENGTH) {
+    if (item.length > MAX_HASHTAG_LENGTH) {
       hashtagInput.setCustomValidity(`Хэштег не должен превышать ${MAX_HASHTAG_LENGTH} символов`);
+    }
+    else if (isHashtagEqual(hashtags)) {
+      hashtagInput.setCustomValidity('Хэштеги не могут повторяться');
     }
     else if (array.length > MAX_HASHTAG_COUNT) {
       hashtagInput.setCustomValidity(`Пожалуйста, не более ${MAX_HASHTAG_COUNT} хэштегов`);
+    }
+    else if (!hashtagPattern.test(item)) {
+      hashtagInput.setCustomValidity('Хэштег должен начинаться с # и содержать только буквы и цифры');
     } else {
       hashtagInput.setCustomValidity('');
     }
@@ -33,6 +45,7 @@ const validateLengthComment = () => {
   commentText.reportValidity();
 };
 
+hashtagInput.addEventListener('input', validateHashtag);
 
 hashtagInput.addEventListener('keydown', (evt) => {
   evt.stopPropagation();
