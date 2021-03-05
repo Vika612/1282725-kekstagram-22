605accd0abb366830b7aede8945994b151a8a9bc
Victoria Domovets <v.domovets@gmail.com>
2021-03-05 12:49:00 +0100

    module6-task2 fix2

--- js/validation.js
+++ js/validation.js
@@ -3,27 +3,21 @@ const MAX_HASHTAG_LENGTH = 20;
 const MAX_COMMENT_LENGTH = 140;
 const hashtagInput = document.querySelector('.text__hashtags');
 const commentText = document.querySelector('.text__description');
-const hashtagPattern = /ˆ#[A-Za-zА-Яа-я0-9]/;
+const hashtagPattern = /^#[A-Za-zА-Яа-я0-9]+$/;
 
-const isHashtagEqual = (hashtags) => {
-  for (let i = 0; i < hashtags.length - 1; i++) {
-    if (hashtags[i] === hashtags[i + 1]) {
-      return true;
-    }
-  }
-};
 
 const validateHashtag = () => {
-  const hashtags = hashtagInput.value.trim().split(' ');
+  const hashtags = hashtagInput.value.trim().toLowerCase().split(' ');
+  const uniqueHashtags = new Set(hashtags);
 
-  hashtags.forEach((item, array) => {
+  hashtags.forEach((item) => {
     if (item.length > MAX_HASHTAG_LENGTH) {
       hashtagInput.setCustomValidity(`Хэштег не должен превышать ${MAX_HASHTAG_LENGTH} символов`);
     }
-    else if (isHashtagEqual(hashtags)) {
+    else if (uniqueHashtags.size !== hashtags.length) {
       hashtagInput.setCustomValidity('Хэштеги не могут повторяться');
     }
-    else if (array.length > MAX_HASHTAG_COUNT) {
+    else if (hashtags.length > MAX_HASHTAG_COUNT) {
       hashtagInput.setCustomValidity(`Пожалуйста, не более ${MAX_HASHTAG_COUNT} хэштегов`);
     }
     else if (!hashtagPattern.test(item)) {
@@ -45,12 +39,14 @@ const validateLengthComment = () => {
   commentText.reportValidity();
 };
 
+
 hashtagInput.addEventListener('input', validateHashtag);
+
+commentText.addEventListener('input', validateLengthComment);
 
 hashtagInput.addEventListener('keydown', (evt) => {
   evt.stopPropagation();
 });
-
 
 commentText.addEventListener('keydown', (evt) => {
   evt.stopPropagation();
